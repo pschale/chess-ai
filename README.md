1ea5eea0bd7dd631f6db8194583e45c637627858
Paul Schale <paul.schale@ligo.org>
2018-04-08 22:15:30 -0700

    added file with all board positions for first 1k games

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 Current status:
 Can take any board position and whose move it is and give all possible positions available from it (equivalent to showing all legal moves, but it's hard to get all moves - like castling - into a uniform format, so I'm just outputing what the board position would be after the move is made). Just added move indicator.
-mostly functional, though not very easy to input moves yet. Features left to implement:
+
+File board_positions_1.csv is a csv file with board positions and castling ability for the first 1000 games in the database, should be ready to stick into a NN
+
+Features left to implement:
 
 En passant:
     can do the move, but won't show up in list of possible moves
--- parsed_games_to_csv.py
+++ parsed_games_to_csv.py
@@ -1,6 +1,7 @@
 import chess_game as cg
 import pandas as pd
 import numpy as np
+import time
 
 b = np.empty((8,8), dtype='str')
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -10,16 +11,20 @@ for i in range(8):
 c = np.empty((8,8), dtype='str')
 for i in range(8):
     c[:, i] = str(i+1)
-    
+
+outfile = open('board_positions_1.txt', 'w+')
+
 squares = np.core.defchararray.add(b,c)
-cols = np.append(squares.flatten(), ['white_tomove', 'w_castle', 'b_castle', 'winner'])
+cols = np.append(squares.flatten(), ['white_tomove', 'w_castle_kingside', 'w_castle_queenside', 'b_castle_kingside', 'b_castle_queenside', 'winner'])
+
+outfile.write("," + ",".join(cols))
 
 i = 0
 
 data = np.empty((0, 70), dtype='str')
 with open('games_parsed.txt', 'r') as h:
     for line in h:
-        print(i)
+        start = time.time()
         game = line.split()
         score = game[0]
         moves = game[1:]
@@ -38,11 +43,14 @@ with open('games_parsed.txt', 'r') as h:
         for m in moves:
             g.move(m)
             board_position = g.to_csv_format()
-            datarow = np.append(board_position, [white_score]).reshape((1, 70))
-            data = np.append(data, datarow, axis=0)
+            #datarow = np.append(board_position, [white_score]).reshape((1, 70))
+            #data = np.append(data, datarow, axis=0)
+            outfile.write("\n")
+            outfile.write(",".join(list(board_position) + [str(white_score)]))
         i += 1
-
-        if i > 1000:
+        end = time.time()
+        print('Game {} done in {:.2f} seconds, {:.4f} sec/move'.format(i, end - start, (end-start)/len(moves)))
+        if i >= 1000:
             break
 df = pd.DataFrame(data=data, columns=cols)
 df.to_csv('board_positions.csv')
